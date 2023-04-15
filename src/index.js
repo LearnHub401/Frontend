a4d07c2488681d9b6d2d9264c6efb4413699d16c
Ty Aponte <aaustinsk8@aol.com>
2023-04-14 20:50:41 -0700

    About page styling and placeholders

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Auth0Provider } from "@auth0/auth0-react";
+// import { Auth0Provider } from "@auth0/auth0-react";
 import { Provider } from 'react-redux';
 import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Auth0Provider
+    {/* <Auth0Provider
       domain={process.env.REACT_APP_AUTH_DOMAIN}
       clientId={process.env.REACT_APP_AUTH_CLIENT_ID}
       redirectUri={process.env.REACT_APP_AUTH_REDIRECT_URI}
-    >
+    > */}
       <Provider store={store}>
         <App />
       </Provider>
-    </Auth0Provider>
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
 
