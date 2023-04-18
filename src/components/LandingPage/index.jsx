dfeba2c8aa844d378340fdd920bd9c0a7840717e
JonathanStaib <JonathanStaibb@gmail.com>
2023-04-18 16:47:05 +0000

    added in mantineProvider

--- src/components/LandingPage/index.jsx
+++ src/components/LandingPage/index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import { Tabs } from "@mantine/core";
 
 const LandingPage = () => {
 
@@ -7,6 +7,18 @@ const LandingPage = () => {
     <>
       <h1>Landing Page</h1>
       <Link to="/courses">Explore Courses</Link>
+
+      <Tabs defaultValue="courses" orientation="vertical">
+      <Tabs.List>
+        <Tabs.Tab value="Arrays">Arrays</Tabs.Tab>
+        <Tabs.Tab value="Trees">Trees</Tabs.Tab>
+        <Tabs.Tab value="Stack">Stack</Tabs.Tab>
+        <Tabs.Tab value="Queue">Queue</Tabs.Tab>
+        <Tabs.Tab value="Redux">Redux</Tabs.Tab>
+        <Tabs.Tab value="SQL">SQL</Tabs.Tab>
+        <Tabs.Tab value="etc">etc...</Tabs.Tab>
+      </Tabs.List>
+      </Tabs>
     </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { MantineProvider } from '@mantine/core'
 import App from './App';
 
 import { Provider } from 'react-redux';
@@ -9,9 +9,11 @@ import store from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <MantineProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MantineProvider>
   </React.StrictMode>
 );
 
