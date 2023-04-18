d07dd4b75daf207b53a6a73d92c2c3906f2687da
JonathanStaib <JonathanStaibb@gmail.com>
2023-04-18 22:05:59 +0000

    added footer about component and profile page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import LandingPage from "./components/LandingPage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./components/Profile";
 import Course from "./components/Course";
+import Footer from "./components/Footer";
+import About from "./components/About";
 import { withAuth0 } from '@auth0/auth0-react';
 
 
@@ -15,7 +17,9 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/courses" element={<Course />} />
+          <Route path="/about" element={<About />} />
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
   );
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,8 @@
+
+const Footer = () => {
+  return(
+    <h4>&copy;Learn Hub 401</h4>
+  )
+}
+
+export default Footer;
