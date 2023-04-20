f2494c3607e8218446516495aeb44007ac0656cb
Marco <pastry07@gmail.com>
2023-04-20 11:56:51 -0700

    added icon to dark mode button

--- src/components/ThemeButton/index.jsx
+++ src/components/ThemeButton/index.jsx
@@ -1,10 +1,11 @@
-import { useMantineColorScheme, Button } from '@mantine/core';
+import { useMantineColorScheme, Button, Center } from '@mantine/core';
+import { IconMoon, IconSun } from '@tabler/icons-react';
 
 function ThemeButton() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
   const dark = colorScheme === 'dark';
   const light = colorScheme === 'light';
-  
+
   const darkButton = {
     backgroundColor: '#31B073',
     color: '#ffffff',
@@ -22,7 +23,24 @@ function ThemeButton() {
 
 
   return (
-    <Button style={dark? lightButton : darkButton} onClick={() => toggleColorScheme()}>{dark ? <p>light</p> : <p>dark</p>}</Button>
+    <Button style={dark ? lightButton : darkButton} onClick={() => toggleColorScheme()}>
+      {
+      dark ? 
+      <>
+        <p style={{paddingRight: '5px'}}>light</p>
+        <Center>
+          <IconSun size="1.05rem" stroke={1.5} />
+        </Center>
+      </>
+      :
+      <>
+        <p style={{paddingRight: '5px'}}>dark </p>
+        <Center>
+          <IconMoon size="1.05rem" stroke={1.5} />
+        </Center>
+      </>
+    }</Button>
+
   );
 }
 
